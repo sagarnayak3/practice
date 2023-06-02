58165c40608a32f14f88475d383a06b3ec4b76a0
sagarnayak3 <sagar.nayak3@live.com>
2023-06-02 11:29:29 +0530

    hii

---
+++ src/test/java/modify/FreequencyTest.java
@@ -0,0 +1,29 @@
+package modify;
+
+public class FreequencyTest {
+	
+	public static void main(String[] args) {
+		
+		
+		int[] a= {12,23,43,12,45,65,23,12,33};
+		int l=a.length;
+		boolean flag[]=new boolean[l];
+		
+		for(int i=0;i<l;i++) {
+			int count=0;
+			if(flag[i]==false) {
+				for(int j=i;j<l;j++) {
+					if(a[i]==a[j]) {
+						count++;
+						flag[j]=true;
+					}//if ends
+				}//j for ends
+				
+			}//if ends
+			if(count!=0)
+			System.out.println(a[i]+" present"+ count+"number of times");
+		}
+		
+	}
+
+}
---
+++ src/test/java/modify/StringFrequency.java
@@ -0,0 +1,33 @@
+package modify;
+
+public class StringFrequency {
+
+		
+		public static void main(String[] args) {
+			
+			
+			String a= "hello my hello  hello name is rahul my name rahul";
+			String[] a1=a.toString().split(" ");
+			int l=a1.length;
+			boolean flag[]=new boolean[l];
+			
+			for(int i=0;i<l;i++) {
+				int count=0;
+				if(flag[i]==false) {
+					for(int j=i;j<l;j++) {
+						if(a1[i].equals(a1[j])) {
+							count++;
+							flag[j]=true;
+						}//if ends
+					}//j for ends
+					
+				}//if ends
+				if(count!=0)
+				System.out.println(a1[i]+" present "+ count+"  number of times");
+			}
+			
+		}
+
+	}
+
+
